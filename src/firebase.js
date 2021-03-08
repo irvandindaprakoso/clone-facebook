7dac56cf1d65eb4cc7157fe9fd4e7383c9cbe7cb
Irvan Dinda Prakoso <irvandindaprakoso@gmail.com>
2021-03-08 07:49:48 +0700

    feat: create header like facebook

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBAUeFdOMe5TFpsl_mWqL390pEdPZ--wNI",
+    authDomain: "facebook-clone-f9ecb.firebaseapp.com",
+    projectId: "facebook-clone-f9ecb",
+    storageBucket: "facebook-clone-f9ecb.appspot.com",
+    messagingSenderId: "625909697876",
+    appId: "1:625909697876:web:e9c5bc2d34a82613e68cc1",
+    measurementId: "G-9Y684JTRHH"
+};
